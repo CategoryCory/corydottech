909b6af67c57b007544a63b29a2aa72e8b4d15b8
Cory Coward <ccoward80@gmail.com>
2024-04-22 22:55:51 -0400

    Add fields to project model, add projects to home page

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.db import models
+from django.utils.text import slugify
 
 from taggit.managers import TaggableManager
 
@@ -13,9 +15,16 @@ class Project(models.Model):
     )
 
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, blank=True, null=True)
+    summary = models.CharField(max_length=255)
     description = models.TextField()
     status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=IN_PROGRESS)
     skills_used = TaggableManager()
 
     def __str__(self) -> str:
         return self.name
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = f'{slugify(self.name)}-{uuid.uuid4()}'
+        super(Project, self).save(*args, **kwargs)
