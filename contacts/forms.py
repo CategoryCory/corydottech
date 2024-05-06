de309d58b8dc86f34310f3351971019ee43dc81e
Cory Coward <ccoward80@gmail.com>
2024-05-06 01:43:23 -0400

    Add exception handling if sending email fails

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,9 +1,11 @@
+import logging
 from django.conf import settings
 from django.core.mail import send_mail
 from django.db import transaction
 from django.forms import ModelForm
 from django_recaptcha.fields import ReCaptchaField
 from django_recaptcha.widgets import ReCaptchaV3
+from smtplib import SMTPException
 
 from .models import Contact
 
@@ -32,13 +34,18 @@ class ContactForm(ModelForm):
                 f'You have a new message from {instance.name} ({instance.email}) on cory.tech.\n\n'
                 f'{instance.message}'
             )
-            send_mail(
-                'New message from cory.tech',
-                message,
-                settings.DEFAULT_FROM_EMAIL,
-                [settings.ADMIN_EMAIL, ],
-                fail_silently=False,
-            )
+            try:
+                send_mail(
+                    'New message from cory.tech',
+                    message,
+                    settings.DEFAULT_FROM_EMAIL,
+                    [settings.ADMIN_EMAIL, ],
+                    fail_silently=False,
+                )
+            except SMTPException as e:
+                logging.error(
+                    f'{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}'
+                )
 
         transaction.on_commit(send_admin_email)
         return instance
