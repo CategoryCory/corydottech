534d429d006965285eeee3bd0bcc55b8b0fdb707
Cory Coward <ccoward80@gmail.com>
2024-05-06 10:24:55 -0400

    Use Sendgrid API

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,11 +1,13 @@
 import logging
 from datetime import datetime
 from django.conf import settings
-from django.core import mail
 from django.db import transaction
 from django.forms import ModelForm
 from django_recaptcha.fields import ReCaptchaField
 from django_recaptcha.widgets import ReCaptchaV3
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from typing import Any
 
 from .models import Contact
 
@@ -28,25 +30,24 @@ class ContactForm(ModelForm):
         fields = ('name', 'email', 'message', 'recaptcha', )
         labels = {'name': 'Name', 'email': 'Email', 'message': 'Message'}
 
-    def save(self, commit=True, *args, **kwargs):
+    def save(self, commit=True, *args, **kwargs) -> Any:
         instance = super(ContactForm, self).save()
 
         def send_admin_email():
-            message: str = (
-                f'You have a new message from {instance.name} ({instance.email}) on cory.tech.\n\n'
-                f'{instance.message}'
+            message_body: str = (
+                f'<p>You have a new message from {instance.name} ({instance.email}) on cory.tech.</p>'
+                f'<p>{instance.message}<p>'
+            )
+            message = Mail(
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                to_emails=settings.ADMIN_EMAIL,
+                subject='New message from cory.tech',
+                html_content=message_body,
             )
             try:
-                mails_sent: int = mail.send_mail(
-                    subject='New message from cory.tech',
-                    message=message,
-                    from_email=settings.DEFAULT_FROM_EMAIL,
-                    recipient_list=[settings.ADMIN_EMAIL],
-                    fail_silently=False,
-                )
-
-                if mails_sent < 1:
-                    logger.warning(f'Attempt to send mail failed at {datetime.now()}')
+                sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+                response = sg.send(message)
+                logger.info(f'Email sent at {datetime.now()} with status {response.status_code}')
             except Exception as e:
                 logger.error(
                     f'{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}'
