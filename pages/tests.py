fa6f8a099d08bd0149157da8ee6fc70bd5b47231
Cory Coward <ccoward80@gmail.com>
2024-05-14 01:46:21 -0400

    Add unit test

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .views import home
+
+
+class HomeViewTest(TestCase):
+
+    def test_home_page_loads(self):
+        response = self.client.get(reverse('pages:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/home.html')
